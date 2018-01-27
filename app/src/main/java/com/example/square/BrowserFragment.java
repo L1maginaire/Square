f6034365111e61a5001ec0f848e3c4eafde1785f
l1maginaire <l1maginaire00@gmail.com>
2018-01-27 15:45:12 +0200

    WebView_progressBar_mapping_fix

--- app/src/main/java/com/example/square/BrowserFragment.java
+++ app/src/main/java/com/example/square/BrowserFragment.java
@@ -8,16 +8,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class BrowserFragment extends Fragment {
     public static final String ARG_URI = "url";
     private Uri mUri;
-    private WebView webView;
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static BrowserFragment newInstance(Uri uri) {
         Bundle args = new Bundle();
@@ -37,14 +41,32 @@ public class BrowserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.webview, container, false);
-        webView = (WebView) v.findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient(){
-            public boolean shouldOverrideUrlLoading(WebView view, String url){
+        mProgressBar = (ProgressBar) v.findViewById(R.id.webProgress);
+        mProgressBar.setMax(100);
+
+        mWebView = (WebView) v.findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
+        mWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 return false;
             }
         });
-        webView.loadUrl(mUri.toString());
+        mWebView.loadUrl(mUri.toString());
         return v;
     }
 }
